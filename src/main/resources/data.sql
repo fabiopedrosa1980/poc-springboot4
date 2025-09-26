f7032c8c3a4bb621fe252dd6879f3a2ce9e610da
Fabio Pedrosa <bito.pedrosa@gmail.com>
2025-09-26 00:47:39 -0300

    Ajustes hexagonal

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,3 @@
-create table if not exists Product(
-    id serial primary key,
-    name varchar not null
-);
 DELETE FROM Product;
 INSERT INTO Product (id,name) values (1,'Iphone 17');
 INSERT INTO Product (id,name) values (2,'Mackbook Pro');
